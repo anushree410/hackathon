7e1f3629d01c14b7b697c11f38a7e62ed91462a0
anushree410 <anudeota@gmail.com>
2024-07-19 10:35:17 +0530

    SignupResponse

---
+++ src/main/java/com/example/hackathon/dto/SignupResponse.java
@@ -0,0 +1,13 @@
+package com.example.hackathon.dto;
+
+public class SignupResponse {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
